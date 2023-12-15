5403d7632028db4a3f4abd96f0cbb5da9ea67c51
II-Paulus-II <paulus.coding@gmail.com>
2023-12-15 11:33:14 +0000

    improved css slightly and added aria-live tag

--- js/app.js
+++ js/app.js
@@ -1,15 +1,29 @@
 console.log("its working");
 
 const thumbnailButton = document.querySelectorAll(".imageThumbnails img");
-console.log(thumbnailButton);
+// console.log(thumbnailButton);
 const imageThing = document.getElementById("imageDisplayed");
-console.log(imageThing);
+// console.log(imageThing);
+const audio = document.querySelector("audio"); //Only one
 
 thumbnailButton.forEach(function (thumb) {
   thumb.addEventListener("click", function () {
-    imageThing.srcset = thumb.srcset;
-    imageThing.src = thumb.src;
-    imageThing.alt = thumb.alt;
-    imageThing.parentElement.querySelector('.mainDesc').innerHTML = thumb.parentElement.querySelector('.imageDesc').innerHTML;
+    displayImage(thumb);
   });
+  thumb.addEventListener("keydown", function (event) {
+    if (event.key === 'Enter') {
+      displayImage(thumb);
+    }
+  })
 });
+
+function displayImage(thumb) {
+  imageThing.srcset = thumb.srcset;
+  imageThing.src = thumb.src;
+  imageThing.alt = thumb.alt;
+  document.getElementById('announcer').textContent = thumb.alt;
+  imageThing.parentElement.querySelector('.mainDesc').innerHTML = thumb.parentElement.querySelector('.imageDesc').innerHTML;
+  if(thumb.alt === "This is a very dangerous cow. You should be afraid of it.") {
+    audio.play();
+  }
+}
