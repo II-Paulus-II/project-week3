c940fcd25a2615f4eb0dada315d001feb4b88c92
II-Paulus-II <paulus.coding@gmail.com>
2023-12-15 14:08:09 +0000

    favicon sorted arrow keys for navigation of images

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
 console.log("its working");
 
+/* ----- Basic Page data and elements ----- */
+
 const thumbnailButton = document.querySelectorAll(".imageThumbnails img");
+const imagesArrayLength = thumbnailButton.length;
+// console.log(imagesArrayLength);
 // console.log(thumbnailButton);
 const imageThing = document.getElementById("imageDisplayed");
 // console.log(imageThing);
 const audio = document.querySelector("audio"); //Only one
+const getNextImgBtn = document.getElementById("next");
+const getPrevImgBtn = document.getElementById("previous");
 
-thumbnailButton.forEach(function (thumb) {
-  thumb.addEventListener("click", function () {
-    displayImage(thumb);
-  });
-  thumb.addEventListener("keydown", function (event) {
-    if (event.key === 'Enter') {
-      displayImage(thumb);
-    }
-  })
-});
+let currentImageIndex = 0;
 
-function displayImage(thumb) {
+/* ----- Basic Page Functions ----- */
+
+function displayImage(thumb, index) {
   imageThing.srcset = thumb.srcset;
   imageThing.src = thumb.src;
   imageThing.alt = thumb.alt;
@@ -26,4 +25,46 @@ function displayImage(thumb) {
   if(thumb.alt === "This is a very dangerous cow. You should be afraid of it.") {
     audio.play();
   }
+  // console.log(index);
+  currentImageIndex = index;
 }
+
+function getNextImage(value) {
+  let newIndex = currentImageIndex + value;
+  // console.log("new index is", newIndex);
+  let nextimage = thumbnailButton[newIndex];
+  if (newIndex >= 0 && newIndex < imagesArrayLength) {
+    displayImage(nextimage, newIndex);
+  }
+}
+
+function arrowKeyFunction(event) {
+  if (event.key === 'ArrowRight' || event.key === 'ArrowDown') {
+      getNextImage(1);
+  } else if (event.key === 'ArrowLeft' || event.key === 'ArrowUp') {
+      getNextImage(-1);
+  }
+}
+
+/* ----- Event Listeners ----- */
+
+thumbnailButton.forEach(function (thumb, index) {
+  thumb.addEventListener("click", function () {
+    displayImage(thumb, index);
+  });
+  thumb.addEventListener("keydown", function (event) {
+    if (event.key === 'Enter') {
+      displayImage(thumb, index);
+    }
+  })
+});
+
+getNextImgBtn.addEventListener("click", function () {
+  getNextImage(1);
+});
+
+getPrevImgBtn.addEventListener("click", function () {
+  getNextImage(-1);
+});
+
+window.addEventListener('keydown', arrowKeyFunction);
